a789673af94ada51cfd23f0b106ffa1967a2512f
Ankush Gedam <Ankush.Gedam@citiustech.com>
2021-09-16 23:06:48 +0530

    Admin Service added

--- src/main/java/com/ct/admin/AdminServiceApplication.java
+++ src/main/java/com/ct/admin/AdminServiceApplication.java
@@ -1,13 +1,25 @@
 package com.ct.admin;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class AdminServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AdminServiceApplication.class, args);
+		
 	}
-
+	
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 }
